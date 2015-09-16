23f3c8acf012a1e80b37756c94b4e06cba94d564
Doug Walter <doug@kosmojo.com>
2015-09-16 15:51:17 -0600

    working demo

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
-var express = require('express'),
-		app = express();
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-app.set('port', process.env.port || 3000);
+app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
 	res.sendFile('index.html')
 });
 
+app.get('/comments.json', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/comments.json', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(comments);
+    });
+  });
+});
 
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
